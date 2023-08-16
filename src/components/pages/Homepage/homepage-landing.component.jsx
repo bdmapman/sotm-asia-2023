b88bf8a3359c3efce4fe8ed33e08b39f6db3c0e6
bdmapman <bdmapman@gmail.com>
2023-08-16 11:38:13 +0600

    Landing Overlay Opacity Correction

--- src/components/pages/Homepage/homepage-landing.component.jsx
+++ src/components/pages/Homepage/homepage-landing.component.jsx
@@ -6,12 +6,7 @@ import { CountdownTimer } from "../../contents/countdown/CountdownTimer";
 import waveBlueBottom from "../../../assets/bg-svg/bg-footer-wave.svg";
 import PageHeaderWave from "../../../assets/bg-svg/page-header-wave.svg";
 import moment from "moment";
-import { 
-    FacebookFilled, 
-    LinkedinFilled, 
-    TwitterCircleFilled, 
-    GithubFilled,
-} from "@ant-design/icons";
+import { FacebookFilled, LinkedinFilled, TwitterCircleFilled, GithubFilled } from "@ant-design/icons";
 
 export const HomepageLanding = (props) => {
     const contents = props.contents;
@@ -22,40 +17,39 @@ export const HomepageLanding = (props) => {
     };
     return (
         <div className="landing-image" style={{ backgroundImage: `url(${LandingImage})` }}>
-            <div className="landing-overlay-gradient">
-                <div className="landing-header-container">
-                    <div className="landing-header">{contents.header}</div>
-                    <p className="landing-sub-header">
-                        {contents.date}
-                        <br />
-                        {contents.venue}
-                    </p>
-                    <p className="landing-sub-header" style={{ fontSize: 20 }}>
-                        {contents.coOrganiser}
-                    </p>
-                    <div className="landing-link">
-                        <text className="landing-link-text">{contents.link}</text>
-                    </div>
-                    <div className="countdown-timer-container">
-                        <CountdownTimer targetDate={programDate} style={{ maxWidth: 360 }} />
-                    </div>
+            <div className="landing-overlay-gradient" />
+            <div className="landing-header-container">
+                <div className="landing-header">{contents.header}</div>
+                <p className="landing-sub-header">
+                    {contents.date}
+                    <br />
+                    {contents.venue}
+                </p>
+                <p className="landing-sub-header" style={{ fontSize: 20 }}>
+                    {contents.coOrganiser}
+                </p>
+                <div className="landing-link">
+                    <text className="landing-link-text">{contents.link}</text>
+                </div>
+                <div className="countdown-timer-container">
+                    <CountdownTimer targetDate={programDate} style={{ maxWidth: 360 }} />
+                </div>
 
-                    <p className="landing-sub-header" style={{ fontSize: 20 }}>
-                        Find more on
-                    </p>
-                    <div className="social-icon-containrer">
-                        <div className="social-icon" style={{backgroundColor: '#4267B2'}}>
-                            <FacebookFilled style={{fontSize:'32px', color: "white"}}/>
-                        </div>
-                        <div className="social-icon" style={{backgroundColor: '#0A66C2'}}>
-                            <LinkedinFilled  style={{fontSize:'32px', color: "white"}}/>
-                        </div>
-                        <div className="social-icon" style={{backgroundColor: '#00acee'}}>
-                            <TwitterCircleFilled style={{fontSize:'32px', color: "white"}}/>
-                        </div>
-                        <div className="social-icon" style={{backgroundColor: '#171515'}}>
-                            <GithubFilled style={{fontSize:'32px', color: "white"}}/>
-                        </div>
+                <p className="landing-sub-header" style={{ fontSize: 20 }}>
+                    Find more on
+                </p>
+                <div className="social-icon-containrer">
+                    <div className="social-icon" style={{ backgroundColor: "#4267B2" }}>
+                        <FacebookFilled style={{ fontSize: "32px", color: "white" }} />
+                    </div>
+                    <div className="social-icon" style={{ backgroundColor: "#0A66C2" }}>
+                        <LinkedinFilled style={{ fontSize: "32px", color: "white" }} />
+                    </div>
+                    <div className="social-icon" style={{ backgroundColor: "#00acee" }}>
+                        <TwitterCircleFilled style={{ fontSize: "32px", color: "white" }} />
+                    </div>
+                    <div className="social-icon" style={{ backgroundColor: "#171515" }}>
+                        <GithubFilled style={{ fontSize: "32px", color: "white" }} />
                     </div>
                 </div>
             </div>
