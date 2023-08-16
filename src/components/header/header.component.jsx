c9e887d29c87d154a7a60bd3807edfde7e1e8b1b
bdmapman <bdmapman@gmail.com>
2023-08-16 11:29:54 +0600

    Update header.component.jsx

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,24 +1,28 @@
 import React, { useState } from "react";
 import { Menu, Layout, Col, Row } from "antd";
 import logo from "../../assets/sotm-asia-2023-logo.png";
-import MenuItems from './menu-items'
-import './header.styles.css'
-import { useNavigate } from 'react-router-dom'
+import MenuItems from "./menu-items";
+import "./header.styles.css";
+import { useNavigate } from "react-router-dom";
+import useViewport from "../../hooks/useViewport";
+import { AlignRightOutlined } from '@ant-design/icons'
 
 export const HeaderComponent = () => {
     const [current, setCurrent] = useState("/");
-    const navigate = useNavigate()
+    const navigate = useNavigate();
     const onClick = (e) => {
         console.log("click ", e);
         setCurrent(e.key);
-        navigate(e.key)
+        navigate(e.key);
     };
+    const { width } = useViewport();
+    const breakpoint = 780;
     return (
-        <Layout style={{width: '100%'}}>
+        <Layout style={{ width: "100%" }}>
             <Row
                 style={{
                     // backgroundColor: "rgba(37, 34, 82, 1)",
-                    backgroundColor: '#ffffff',
+                    backgroundColor: "#ffffff",
                     display: "flex",
                     alignItems: "center",
                 }}
@@ -30,16 +34,17 @@ export const HeaderComponent = () => {
                         float: "right",
                     }}
                 >
-                    <Menu 
-                        theme='light'
+                    <Menu
+                        theme="light"
                         onClick={onClick}
                         selectedKeys={[current]}
                         mode="horizontal"
+                        overflowedIndicator={<AlignRightOutlined />}
                         items={MenuItems}
                         style={{
                             fontFamily: "'Barlow Semi Condensed', sans-serif",
                             fontWeight: 500,
-                            fontSize: 'large',
+                            fontSize: "large",
                             backgroundColor: "rgba(0,0,0,0)",
                             display: "flex",
                             justifyContent: "flex-end",
@@ -58,7 +63,15 @@ export const HeaderComponent = () => {
                     }}
                 >
                     <img src={logo} alt="logo" height={100} style={{ marginLeft: 1 }} />
-                    <p className="text-logo"><span className="alternate-logo-color">State of the Map</span><br />Asia - 2023</p>
+                    {width > breakpoint ? (
+                        <p className="text-logo">
+                            <span className="alternate-logo-color">State of the Map</span>
+                            <br />
+                            Asia - 2023
+                        </p>
+                    ) : (
+                        <div></div>
+                    )}
                 </Col>
             </Row>
         </Layout>
